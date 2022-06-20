fac008a4aa632e59a2714fbf881a9a624f3e254b
hibohiboo <swdice66@gmail.com>
2022-06-21 00:31:24 +0900

    fix all user

--- client/src/domain/http/mocks/handlers.ts
+++ client/src/domain/http/mocks/handlers.ts
@@ -13,7 +13,21 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(req))
   }),
   rest.get(`${domain}get-users-value`, (req, res, ctx) => {
+    // fetchの場合
     // http://localhost:3000/get-users-value?from=2022-07-01&to=2022-07-03&users=1%2C2
+    // axiosの場合
+    // http://localhost:3000/get-users-value?from=2022-07-01&to=2022-07-03&users[]=1&users[]=2
+
+    const from = req.url.searchParams.get('from')
+    const to = req.url.searchParams.get('to')
+    const users = req.url.searchParams.getAll('users[]') || []
+    if (typeof from === 'string' && typeof to === 'string') {
+      const ret = users.flatMap((userId) =>
+        getData(new Date(from), new Date(to)).map((i) => ({ ...i, userId })),
+      )
+      return res(ctx.status(200), ctx.json(ret))
+    }
+
     return res(ctx.status(200), ctx.json(req))
   }),
 ]
